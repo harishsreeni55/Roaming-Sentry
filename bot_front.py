ce0f1f32b3280e1e8fc98526b4c894b3091421cd
harishsreeni55 <84909518+harishsreeni55@users.noreply.github.com>
2023-03-23 17:38:52 +0530

    Add files via upload

--- bot_front.py
+++ bot_front.py
@@ -10,7 +10,7 @@ def main():
         kobuki_instance.move(0, 0, 0)
         t2 = time.time()
         while(time.time()-t2<=3):
-            kobuki_instance.move(-255, 255, 1)
+            kobuki_instance.move(0, 255, 0)
         kobuki_instance.move(0, 0, 0)
 
 main()
